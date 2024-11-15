dbfa522a7b6c4e047d92511f483cceb8db9b26d0
Jaqueline <jaquelineeviana@gmail.com>
2024-11-15 17:47:09 -0300

    Implementado o método de transferencia.

--- src/main/java/com/bancoapi/banco_api/controllers/ContaController.java
+++ src/main/java/com/bancoapi/banco_api/controllers/ContaController.java
@@ -1,5 +1,7 @@
 package com.bancoapi.banco_api.controllers;
 
+import com.bancoapi.banco_api.dto.TransferenciaRequest;
+import com.bancoapi.banco_api.dto.TransferenciaResponse;
 import com.bancoapi.banco_api.models.Conta;
 import com.bancoapi.banco_api.services.ContaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,33 +23,42 @@ public class ContaController {
         return ResponseEntity.ok(novaConta);
     }
 
-        @PutMapping("/{id}/rendimento")
+    @PutMapping("/{id}/rendimento")
     public ResponseEntity<String> aplicarRendimentoNaConta (@PathVariable Long id) {
         contaService.aplicarRendimento(id);
         return ResponseEntity.ok("Rendimento aplicado com sucesso");
     }
 
-    @GetMapping //define o endpoint para buscar e listar.
+    @GetMapping
     public List<Conta> listarContas () {
         return contaService.listarContas();
     }
 
-    @GetMapping("/{id}") //define o endpoint para buscar contas especificas.
+    @GetMapping("/{id}")
     public Conta obterConta (@PathVariable Long id) { //@PathVariable Usa valores que estão na URL diretamente (como IDs).
         return contaService.obterConta(id);
     }
 
-    @PutMapping("/{id}/deposito") //define o endpoint para depositar.
+    @PutMapping("/{id}/deposito")
     public Conta depositar (@PathVariable Long id, @RequestParam double valor) { //@RequestParam Usa valores que estão após ? (como parâmetros de consulta).
         return contaService.depositar(id, valor);
     }
 
-    @PutMapping("/{id}/sacar") //define o endpoint para sacar.
+    @PutMapping("/{id}/sacar")
     public Conta sacar (@PathVariable Long id, @RequestParam double valor) {
         return contaService.sacar(id, valor);
     }
 
-    @DeleteMapping("/{id}") //endpoint para deletar uma conta
+    @PutMapping("/transferir")
+    public TransferenciaResponse transferir (@RequestBody TransferenciaRequest transferenciaRequest) {
+        return contaService.transferir(
+                transferenciaRequest.getIdOrigem(),
+                transferenciaRequest.getIdDestino(),
+                transferenciaRequest.getValor()
+        );
+    }
+
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> encerrarConta (@PathVariable Long id) {
          contaService.encerrarConta(id);
          return ResponseEntity.ok().build();
--- src/main/java/com/bancoapi/banco_api/services/ContaService.java
+++ src/main/java/com/bancoapi/banco_api/services/ContaService.java
@@ -1,6 +1,7 @@
 package com.bancoapi.banco_api.services;
 
 
+import com.bancoapi.banco_api.dto.TransferenciaResponse;
 import com.bancoapi.banco_api.models.Conta;
 import com.bancoapi.banco_api.models.TipoConta;
 import com.bancoapi.banco_api.repositories.ContaRepository;
@@ -14,6 +15,8 @@ public class ContaService {
 
     @Autowired
     private ContaRepository contaRepository;
+//    @Autowired
+//    private TransferenciaResponse response;
 
     public Conta criarConta(Conta conta) {
 
@@ -36,29 +39,61 @@ public class ContaService {
         }
     }
 
-    public List<Conta> listarContas () {
+    public List<Conta> listarContas() {
         return contaRepository.findAll();
     }
 
-    public Conta obterConta (Long id) {
+    public Conta obterConta(Long id) {
         return contaRepository.findById(id)
-                .orElseThrow(()-> new IllegalArgumentException("Conta não encontrada"));
+                .orElseThrow(() -> new IllegalArgumentException("Conta não encontrada"));
     }
 
-    public Conta depositar (Long id, double valor) {
+    public Conta depositar(Long id, double valor) {
         Conta conta = obterConta(id);
         conta.setSaldo(conta.getSaldo() + valor);
         return contaRepository.save(conta);
     }
 
-    public Conta sacar (Long id, double valor) {
+    public Conta sacar(Long id, double valor) {
         Conta conta = obterConta(id);
         if (conta.getSaldo() >= valor) {
             conta.setSaldo(conta.getSaldo() - valor);
-           return contaRepository.save(conta);
+            return contaRepository.save(conta);
         }
         throw new IllegalArgumentException("Saldo insuficiente");
     }
+
+    public TransferenciaResponse transferir(Long idOrigem, Long idDestino, double valor) {
+        Conta contaOrigem = obterConta(idOrigem);
+        Conta contaDestino = obterConta(idDestino);
+
+        if (contaOrigem == null || contaDestino == null) {
+            throw new IllegalArgumentException("Conta de origem ou destino inexistente");
+        }
+
+        if (valor <= 0) {
+            throw new IllegalArgumentException("O valor da transferência deve ser maior que zero");
+        }
+
+        if (contaOrigem.getSaldo() < valor) {
+            throw new IllegalArgumentException("Saldo insufuciente");
+        }
+
+        contaOrigem.setSaldo(contaOrigem.getSaldo() - valor);
+        contaDestino.setSaldo(contaDestino.getSaldo() + valor);
+
+        contaRepository.save(contaOrigem);
+        contaRepository.save(contaDestino);
+
+        TransferenciaResponse response = new TransferenciaResponse();
+        response.setContaOrigem(contaOrigem);
+        response.setContaDestino(contaDestino);
+        response.setValor(valor);
+        response.setStatus("Transferência realizada com sucesso!");
+
+        return response;
+    }
+
 
     public void encerrarConta (Long id) {
         Conta conta = obterConta(id);
